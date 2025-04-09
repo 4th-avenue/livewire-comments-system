d2ccb2995d146256c73db0f49a1ecd2bcd1b25f8
4th-avenue <quebec706@gmail.com>
2025-04-09 20:39:48 +0900

    Article과 Episode 컨트롤러 생성하고 Route

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\EpisodeController;
 
 Route::view('/', 'welcome');
 
@@ -11,5 +13,10 @@ Route::view('dashboard', 'dashboard')
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/articles/{article:slug}', ArticleController::class)->name('articles.show');
+    Route::get('/episodes/{episode:slug}', EpisodeController::class)->name('episodes.show');
+});
 
 require __DIR__.'/auth.php';
