b0979664db7d653a081b0f70a94ba9306a1a7b5e
4th-avenue <quebec706@gmail.com>
2025-04-09 19:45:58 +0900

    articles와 episodes 테이블에 컬럼들 추가하고 마이그레이션 실행

--- src/database/migrations/2025_04_09_091459_create_articles_table.php
+++ src/database/migrations/2025_04_09_091459_create_articles_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
             $table->timestamps();
         });
     }
