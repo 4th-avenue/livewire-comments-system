68d3f72354be83a7767709b35c209c8042fc072a
4th-avenue <quebec706@gmail.com>
2025-04-12 13:57:18 +0900

    리팩터링 (LIVEWIRE Comment 컴포넌트 생성해서 Comments 컴포넌트의 역할 분담)

--- src/resources/views/livewire/comments.blade.php
+++ src/resources/views/livewire/comments.blade.php
@@ -8,66 +8,7 @@
 
         {{-- Comment Body --}}
         @foreach ($comments as $comment)
-        <div>
-            <article class="my-6 text-base bg-white rounded-lg">
-                <footer class="flex justify-between items-center mb-2">
-                    <div class="flex items-center">
-                        <p class="inline-flex items-center mr-3 text-sm text-gray-900">
-                            <img src="{{ $comment->user->avatar() }}" alt="{{ $comment->user->name }}" class="mr-2 w-6 h-6 rounded-full">
-                            {{ $comment->user->name }}
-                        </p>
-
-                        <p class="text-sm text-gray-600">
-                            {{ $comment->created_at->diffForHumans() }}
-                        </p>
-                    </div>
-                </footer>
-
-                <!-- Comment Body -->
-                <p class="text-gray-500">
-                    {{ $comment->body }}
-                </p>
-
-                <!-- Reply, Edit, Delete Section -->
-                <div class="flex items-center mt-4 space-x-4">
-                    <button type="button" class="flex items-center text-sm text-gray-500 hover:underline">
-                        <svg aria-hidden="true" class="mr-1 w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"></path>
-                        </svg>
-                        Reply
-                    </button>
-
-                    <button type="button" class="flex items-center text-sm text-gray-500 hover:underline">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="mr-1 size-4">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
-                        </svg>
-                        Edit
-                    </button>
-
-                    <button type="button" class="flex items-center text-sm text-gray-500 hover:underline">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="mr-1 size-4">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                        </svg>
-                        Delete
-                    </button>
-                </div>
-
-                {{-- <form wire:submit="replyComment" class="mb-6 mt-6 ml-6">
-                    <div class="py-2 px-4 mb-4 bg-white rounded-lg rounded-t-lg border border-gray-200">
-                        <label for="comment" class="sr-only">Your comment</label>
-                        <textarea id="comment" style="resize: none;" placeholder="Write a comment..." class="px-0 w-full text-sm text-gray-900 border-0 focus:ring-0 focus:outline-none"></textarea>
-                    </div>
-
-                    <button type="submit" class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-primary-200 hover:bg-blue-800">
-                        Reply
-                    </button>
-
-                    <button class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center border-gray-300 text-gray-700 bg-white hover:bg-gray-50 rounded-lg">
-                        Cancel
-                    </button>
-                </form> --}}
-            </article>
-        </div>
+            <livewire:comment :$comment :key="$comment->id" />
         @endforeach
 
         {{-- Replies --}}
