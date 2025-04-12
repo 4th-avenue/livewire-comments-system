5944dd8184221a11399d7d5dd31c2c76d7476549
4th-avenue <quebec706@gmail.com>
2025-04-12 11:24:48 +0900

    Comment 모델과 User 모델의 릴레이션 설정 및 scopeParent와 avatar 메서드 생성

--- src/app/Models/Comment.php
+++ src/app/Models/Comment.php
@@ -3,12 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Comment extends Model
 {
     use SoftDeletes;
+
+    public function scopeParent(Builder $query): Builder
+    {
+        return $query->whereNull('parent_id');
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 
     public function commentable(): MorphTo
     {
