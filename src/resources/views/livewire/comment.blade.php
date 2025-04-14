19926be810e3ca86d12f0b3ed659a0be9fe2f29e
4th-avenue <quebec706@gmail.com>
2025-04-14 16:47:49 +0900

    Reply 버튼 클릭하면 대댓글 작성 필드 나타나게 하기 (Toggle)

--- src/resources/views/livewire/comment.blade.php
+++ src/resources/views/livewire/comment.blade.php
@@ -1,4 +1,4 @@
-<div>
+<div x-data="{ isReplying: false }">
     <article class="my-6 text-base bg-white rounded-lg">
         <footer class="flex justify-between items-center mb-2">
             <div class="flex items-center">
@@ -21,7 +21,7 @@
         <!-- Reply, Edit, Delete Section -->
         <div class="flex items-center mt-4 space-x-4">
             @if (!$comment->parent_id)
-                <button type="button" class="flex items-center text-sm text-gray-500 hover:underline">
+                <button @click="isReplying=!isReplying" type="button" class="flex items-center text-sm text-gray-500 hover:underline">
                     <svg aria-hidden="true" class="mr-1 w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"></path>
                     </svg>
@@ -44,6 +44,20 @@
             </button>
         </div>
     </article>
+
+    {{-- Reply form --}}
+    <form class="mb-6 ml-8 lg:ml-12" x-show="isReplying" x-transition>
+        <label for="comment" class="sr-only">Your comment</label>
+        <textarea id="comment" style="resize: none;" placeholder="Write a reply..." rows="2" class="shadow-sm block rounded-md w-full border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500"></textarea>
+
+        <button type="submit" class="inline-flex items-center py-1.5 px-3 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-primary-200 hover:bg-blue-800 mt-4">
+            Reply
+        </button>
+
+        <button @click="isReplying=!isReplying" type="button" class="py-1.5 px-3 text-xs font-medium text-gray-900 shadow-sm ring-1 ring-inset bg-white ring-gray-300 hover:bg-gray-50 rounded-lg">
+            Cancel
+        </button>
+    </form>
 
     <div class="mt-6 ml-8 lg:ml-12">
         @foreach ($comment->replies as $reply)
