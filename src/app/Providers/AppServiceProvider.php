66fa3cc033e9b639f1bc6624ea4bb5d1f8358593
4th-avenue <quebec706@gmail.com>
2025-05-24 15:05:47 +0900

    Eloquent 다형성 관계에 morphMap 도입 및 관련 설정 추가 (Article, Episode 모델에 alias 적용)

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Relations\Relation;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,5 +22,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Model::preventLazyLoading();
+
+        Relation::enforceMorphMap([
+            'Article' => \App\Models\Article::class,
+            'Episode' => \App\Models\Episode::class,
+        ]);
     }
 }
